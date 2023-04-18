0d00fefd657f3beaf44d31227b2ad1ea15db47b3
tbgolfer86 <tbj86@outlook.com>
2023-04-18 17:53:06 -0400

    removed unnecessary files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
-const friendRoutes = require('./friendRoutes');
-const reactionRoutes = require('./reactionRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/friends', friendRoutes);
-router.use('/reactions', reactionRoutes);
 router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
 

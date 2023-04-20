4a92092c628739db6e02df2d4339ba98da4c940d
tbgolfer86 <tbj86@outlook.com>
2023-04-20 16:25:02 -0400

    added user CRUD controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,20 +3,49 @@ const { User, Thought } = require('../models');
 module.exports = {
     getAllUsers(req, res) {
         User.find()
-            .then((courses) => res.json(courses))
+            .then((users) => res.json(users))
             .catch((err) => res.status(500).json(err));
     },
     getUserById(req, res) {
-        
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
     },
     createUser(req, res) {
-        
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+        });
     },
     updateUser(req, res) {
-        
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+          )
+            .then((user) =>
+              !user
+                ? res.status(404).json({ message: 'No user with this id!' })
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
     },
     deleteUser(req, res) {
-        
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : Thought.deleteMany({ _id: { $in: user.thoughts } })
+            )
+            .then(() => res.json({ message: 'User and thoughts deleted!' }))
+            .catch((err) => res.status(500).json(err));
     },
     addFriend(req, res) {
         

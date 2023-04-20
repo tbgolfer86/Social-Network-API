8dca8df71c4437fee6f978b3fcbb6ea3b8ebe882
tbgolfer86 <tbj86@outlook.com>
2023-04-20 14:52:35 -0400

    set up functions in controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,25 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    getAllThoughts(req, res) {
+        
+    },
+    getThoughtById(req, res) {
+        
+    },
+    createThought(req, res) {
+        
+    },
+    updateThought(req, res) {
+        
+    },
+    deleteThought(req, res) {
+        
+    },
+    addReaction(req, res) {
+        
+    },
+    removeReaction(req, res) {
+        
+    }
+}
---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    getAllUsers(req, res) {
+        
+    },
+    getUserById(req, res) {
+        
+    },
+    createUser(req, res) {
+        
+    },
+    updateUser(req, res) {
+        
+    },
+    deleteUser(req, res) {
+        
+    },
+    addFriend(req, res) {
+        
+    },
+    removeFriend(req, res) {
+        
+    }
+}

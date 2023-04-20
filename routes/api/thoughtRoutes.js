f68bc4bb7bf065a8beba391b791dc101feba3a91
tbgolfer86 <tbj86@outlook.com>
2023-04-20 14:41:33 -0400

    added user and thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,27 @@
 const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+router.route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+    .post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction);
+    
+module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,27 @@
 const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/userController');
+
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router.route('/:userId')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router.route('/:userId/friends')
+    .post(addFriend);
+
+router.route('/:userId/friends/:friendId')
+    .delete(removeFriend);
+    
+module.exports = router;
